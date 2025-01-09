1f19f597a704b585015031a28639a1730c4678c1
naomi-rozen <nomirose0@gmail.com>
2025-01-09 15:15:40 +0200

    completed symbol table

--- SymbolTable.py
+++ SymbolTable.py
@@ -16,15 +16,23 @@ class SymbolTable:
 
     def __init__(self) -> None:
         """Creates a new empty symbol table."""
-        # Your code goes here!
-        pass
+        self.class_table = {}
+        self.subroutine_table = {}
+        self.field_count = 0
+        self.static_count = 0
+        self.arg_count = 0
+        self.local_count = 0
+    
+
 
     def start_subroutine(self) -> None:
         """Starts a new subroutine scope (i.e., resets the subroutine's 
         symbol table).
         """
-        # Your code goes here!
-        pass
+        self.subroutine_table = {}
+        self.arg_count = 0
+        self.local_count = 0
+        
 
     def define(self, name: str, type: str, kind: str) -> None:
         """Defines a new identifier of a given name, type and kind and assigns 
@@ -37,8 +45,18 @@ class SymbolTable:
             kind (str): the kind of the new identifier, can be:
             "STATIC", "FIELD", "ARG", "VAR".
         """
-        # Your code goes here!
-        pass
+        if kind == "STATIC":
+            self.class_table[name] = {"type": type, "kind": kind, "index": self.static_count}
+            self.static_count += 1
+        elif kind == "FIELD":
+            self.class_table[name] = {"type": type, "kind": kind, "index": self.field_count}
+            self.field_count += 1
+        elif kind == "ARG":
+            self.subroutine_table[name] = {"type": type, "kind": kind, "index": self.arg_count}
+            self.arg_count += 1
+        elif kind == "VAR":
+            self.subroutine_table[name] = {"type": type, "kind": kind, "index": self.local_count}
+            self.local_count += 1
 
     def var_count(self, kind: str) -> int:
         """
@@ -49,8 +67,14 @@ class SymbolTable:
             int: the number of variables of the given kind already defined in 
             the current scope.
         """
-        # Your code goes here!
-        pass
+        if kind == "STATIC":
+            return self.static_count
+        elif kind == "FIELD":
+            return self.field_count
+        elif kind == "ARG":
+            return self.arg_count
+        elif kind == "VAR":
+            return self.local_count
 
     def kind_of(self, name: str) -> str:
         """
@@ -61,8 +85,12 @@ class SymbolTable:
             str: the kind of the named identifier in the current scope, or None
             if the identifier is unknown in the current scope.
         """
-        # Your code goes here!
-        pass
+        if name in self.subroutine_table:
+            return self.subroutine_table[name]["kind"]
+        elif name in self.class_table:
+            return self.class_table[name]["kind"]
+        else:
+            return None
 
     def type_of(self, name: str) -> str:
         """
@@ -72,8 +100,12 @@ class SymbolTable:
         Returns:
             str: the type of the named identifier in the current scope.
         """
-        # Your code goes here!
-        pass
+        if name in self.subroutine_table:
+            return self.subroutine_table[name]["type"]
+        elif name in self.class_table:
+            return self.class_table[name]["type"]
+        else:
+            return None
 
     def index_of(self, name: str) -> int:
         """
@@ -83,5 +115,9 @@ class SymbolTable:
         Returns:
             int: the index assigned to the named identifier.
         """
-        # Your code goes here!
-        pass
+        if name in self.subroutine_table:
+            return self.subroutine_table[name]["index"]
+        elif name in self.class_table:
+            return self.class_table[name]["index"]
+        else:
+            return None
